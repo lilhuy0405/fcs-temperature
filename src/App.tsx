e770c3d225a843850d9acfc7d7c10d7b7f666f01
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-11-15 15:23:35 +0700

    fix responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import TemperatureChart from "./TemperatureChart.tsx";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import moment from "moment";
-import {Progress} from "antd";
+import {Col, Image, Progress, Row} from "antd";
 
 function App() {
+  const [chartSize, setChartSize] = useState({
+    width: 600,
+    height: 400,
+  });
+  const containerRef = useRef<any>();
   const [temperatures, setTemperatures] = useState<any>([]);
   const [latestTemperature, setLatestTemperature] = useState<any>(null);
   console.log('latestTemperature', latestTemperature)
@@ -25,46 +30,68 @@ function App() {
     fetchData();
   }, []);
 
-
+  useEffect(() => {
+    const handleResize = () => {
+      const width = containerRef.current?.offsetWidth;
+      const height = containerRef.current?.offsetHeight;
+      setChartSize({
+        width,
+        height,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
-      <div style={{
-        width: '600px',
-        margin: '0 auto',
-      }}>
-        <div>
-          <h1 className='text-center'>FCS Temperature</h1>
-          <div className='text-center my-3'>
-            Nhiệt độ tầng 4 nhà đk đọc từ cảm biến nhiệt độ và độ ẩm DHT22!
-          </div>
-          <div className='text-center my-3'>
-            <span>Nhiệt độ: {latestTemperature?.temperature} °C | </span>
-            <span>Cảm giác như: {latestTemperature?.heatIndex} °C | </span>
-            <span>Cập nhật lúc: {moment(latestTemperature?.createdAt).format('DD/MM HH:mm')}</span>
-          </div>
-          <TemperatureChart
-            width={600}
-            height={300}
-            temperatures={temperatures}
-            setLatestTemperature={setLatestTemperature}
-          />
+      <Row gutter={24}>
+        <Col lg={8} md={2} sm={0} xs={0}></Col>
+        <Col lg={8} md={20} sm={24} xs={24}>
           <div>
-            <h3 className='text-center my-3'>Độ ẩm</h3>
-            <div className="center-flex">
-              <Progress
-                type={'circle'}
-                size={150}
-                percent={latestTemperature?.humidity}
+            <div ref={containerRef}>
+              <h1 className='text-center'>FCS Temperature</h1>
+              <div className='text-center my-3'>
+                Nhiệt độ tầng 4 nhà đk đọc từ cảm biến nhiệt độ và độ ẩm DHT22!
+              </div>
+              <div className='text-center my-3'>
+                <span>Nhiệt độ: {latestTemperature?.temperature} °C | </span>
+                <span>Cảm giác như: {latestTemperature?.heatIndex} °C | </span>
+                <span>Cập nhật lúc: {moment(latestTemperature?.createdAt).format('DD/MM HH:mm')}</span>
+              </div>
+              <TemperatureChart
+                width={chartSize.width}
+                height={300}
+                temperatures={temperatures}
+                setLatestTemperature={setLatestTemperature}
               />
+              <div>
+                <h3 className='text-center my-3'>Độ ẩm</h3>
+                <div className="center-flex">
+                  <Progress
+                    type={'circle'}
+                    size={150}
+                    percent={latestTemperature?.humidity}
+                  />
+                </div>
+              </div>
+              <div className='my-3'>
+                <Image src='/lilhuy-temperature-bot.jpg' preview={false}/>
+              </div>
+              <div className='text-small text-gray my-3'>
+                Created by <a href="https://github.com/lilhuy0405" target='_blank'>lilhuy</a> with ESP32, DHT22, MQTT,
+                ReactJS, Ant Design
+              </div>
             </div>
-          </div>
-          <div className='text-small text-gray my-3'>
-            Created by <a href="https://github.com/lilhuy0405" target='_blank'>lilhuy</a> with ESP32, DHT22, MQTT,
-            ReactJS, Ant Design
-          </div>
-        </div>
 
-      </div>
+          </div>
+        </Col>
+        <Col lg={8} md={2} sm={0} xs={0}></Col>
+
+      </Row>
+
     </>
   )
 }
