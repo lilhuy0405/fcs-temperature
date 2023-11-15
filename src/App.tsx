598923d3ed6ed05008c19e3f85869182394ea47b
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-11-15 09:30:27 +0700

    publish

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import TemperatureChart from "./TemperatureChart.tsx";
+import {useEffect, useState} from "react";
+import moment from "moment";
+import {Progress} from "antd";
+
+function App() {
+  const [temperatures, setTemperatures] = useState<any>([]);
+  const [currentHumidity, setCurrentHumidity] = useState<any>(null);
+  const fetchData = async () => {
+    try {
+      const url = "http://54.255.62.9:3000/temperature?limit=1000";
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setTemperatures(data.data)
+      setCurrentHumidity(data.data[data.data.length - 1])
+    } catch (err: any) {
+      console.log("err", err);
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const lastTemperature = temperatures[temperatures.length - 1];
+
+  return (
+    <>
+      <div style={{
+        width: '600px',
+        margin: '0 auto',
+      }}>
+        <div>
+          <h1 className='text-center'>FCS Temperature</h1>
+          <div className='text-center my-3'>
+            Nhiệt độ tầng 4 nhà đk đọc từ cảm biến nhiệt độ và độ ẩm DHT22!
+          </div>
+          <div className='text-center my-3'>
+            <span>Nhiệt độ: {lastTemperature?.temperature} °C | </span>
+            <span>Cảm giác như: {lastTemperature?.heatIndex} °C | </span>
+            <span>Cập nhật lúc: {moment(lastTemperature?.createdAt).format('DD/MM HH:MM')}</span>
+          </div>
+          <TemperatureChart
+            width={600}
+            height={300}
+            temperatures={temperatures}
+            setCurrentHumidity={setCurrentHumidity}
+          />
+          <div>
+            <h3 className='text-center my-3'>Độ ẩm</h3>
+            <div className="center-flex">
+              <Progress
+                type={'circle'}
+                size={150}
+                percent={currentHumidity?.humidity}
+              />
+            </div>
+          </div>
+          <div className='text-small text-gray my-3'>
+            Created by <a href="https://github.com/lilhuy0405" target='_blank'>lilhuy</a> with ESP32, DHT22, MQTT,
+            ReactJS, Ant Design
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/TemperatureChart.tsx
@@ -0,0 +1,135 @@
+import {useEffect, useMemo, useRef, useState} from "react";
+import {createChart, CrosshairMode} from "lightweight-charts";
+import moment from "moment";
+//eslint-disable-next-line
+//@ts-ignore
+import mqtt from "mqtt/dist/mqtt.esm";
+
+const TemperatureChart = ({width, height, temperatures, setCurrentHumidity}: any) => {
+
+  const topic = 'esp32/temperature-bot/web'
+  const [currentLine, setCurrentLine] = useState<any>(null)
+
+  const chartData = useMemo(() => {
+    return temperatures.map((item: any) => {
+      const offset = new Date().getTimezoneOffset() * -1;
+      const offsetHours = offset / 60;
+      return {
+        //default time is utc so we need to convert to local time by adding offset
+        time: +moment(item.createdAt).add(offsetHours, 'hours').format('X'),
+        value: +item.temperature,
+      }
+    })
+  }, [temperatures])
+
+  const chartContainerRef = useRef<any>();
+  useEffect(() => {
+    const client = mqtt.connect("wss://emqx.vietcombank.co.uk:8084/mqtt");
+    const chart = createChart(chartContainerRef.current, {
+      width,
+      height,
+      layout: {
+        background: {
+          // type: 'solid',
+          color: "#fff",
+        },
+        // lineColor: '#2B2B43',
+        textColor: '#191919',
+      },
+      grid: {
+        vertLines: {
+          color: "rgba(197, 203, 206, 0.5)",
+        },
+        horzLines: {
+          color: "rgba(197, 203, 206, 0.5)",
+        },
+      },
+      timeScale: {
+        timeVisible: true,
+        secondsVisible: false,
+      },
+      crosshair: {
+        mode: CrosshairMode.Magnet,
+      },
+      rightPriceScale: {
+        borderColor: "rgba(197, 203, 206, 0.8)",
+      },
+    });
+    try {
+      const lineSeries = chart.addAreaSeries({
+        topColor: 'rgba(33, 150, 243, 0.56)',
+        bottomColor: 'rgba(33, 150, 243, 0.04)',
+        lineColor: 'rgba(33, 150, 243, 1)',
+        lineWidth: 2,
+      });
+      lineSeries.setData(chartData);
+
+
+      chart.subscribeCrosshairMove((param) => {
+        if (param.time) {
+          const currentLine: any = param.seriesData.get(lineSeries);
+          setCurrentLine(currentLine);
+        }
+      });
+
+      client.on("connect", () => {
+        console.log("Connected to EMQ server");
+        // Subscribe to topics or perform other actions here
+        client.subscribe(topic);
+      });
+      client.on("error", (error: any) => {
+        console.error("MQTT Error:", error);
+      });
+
+      client.on("close", () => {
+        console.log("Connection to EMQ server closed");
+      });
+      client.on("message", (receivedTopic: any, message: any) => {
+        console.log(
+          `Received message on topic ${receivedTopic}: ${message.toString()}`
+        );
+        // // Handle the incoming message as needed
+        const newBar = JSON.parse(message.toString());
+        console.log("newBar", newBar);
+        if (+chartData.time === +newBar.time) {
+          console.log("duplicate");
+          return;
+        }
+        //if new bar time is less than last bar time then return
+        const lastBar = chartData[chartData.length - 1];
+        if (!lastBar) {
+          return;
+        }
+        if (+lastBar.time > +newBar.time) {
+          return;
+        }
+        setCurrentHumidity(newBar);
+        const offset = new Date().getTimezoneOffset() * -1;
+        const offsetHours = offset / 60;
+        const newBarNormalized: any = {
+          time: +moment(newBar.createdAt).add(offsetHours, 'hours').format('X'),
+          value: +newBar.temperature,
+        }
+        lineSeries.update(newBarNormalized);
+        console.log("add new bar", newBarNormalized)
+
+      });
+    } catch (err: any) {
+      console.log("err", err.message);
+    }
+    return () => {
+      // window.removeEventListener('resize', handleResize);
+      chart.remove();
+    };
+  }, [width, height, chartData]);
+
+  return (
+    <div style={{position: 'relative'}}>
+      <div className='chart-legend'>
+         {currentLine?.value} °C
+      </div>
+      <div ref={chartContainerRef}/>
+    </div>
+  );
+}
+export default TemperatureChart
